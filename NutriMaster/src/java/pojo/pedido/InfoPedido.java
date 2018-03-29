d32479ede569e5b47c4512e58daceaed29e02042
Reni Steffenon <renisteffenon@hotmail.com>
2018-03-28 23:33:44 -0300

    pojo.pedido(3)
    tabela InfoPedido.

---
+++ NutriMaster/src/java/pojo/pedido/InfoPedido.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojo.pedido;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author renisteffenon
+ */
+
+@Entity
+public class InfoPedido {
+    
+    private Long id;
+    private Paciente paciente;
+    private Pedido pedido;
+    private Date dataPedido;
+    private int incidencia;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public int getIncidencia() {
+        return incidencia;
+    }
+
+    public void setIncidencia(int incidencia) {
+        this.incidencia = incidencia;
+    }
+    
+    
+}
