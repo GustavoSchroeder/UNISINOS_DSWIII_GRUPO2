943434cd4b6d3bc61dd6401431bf3362635beb76
gSchroeder <guto_schroeder@hotmail.com>
2018-03-30 23:28:08 -0300

    atualizações na dieta

--- NutriMaster/src/java/controller/dieta/DietaBean.java
+++ NutriMaster/src/java/controller/dieta/DietaBean.java
@@ -2,13 +2,16 @@ package controller.dieta;
 
 import controller.userControl.UsuarioBean;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
+import pojo.paciente.CalendarioAlimentacao;
 import pojo.paciente.Dieta;
 import pojo.usuario.Usuario;
 import util.JPAUtil;
@@ -28,11 +31,23 @@ public class DietaBean implements Serializable {
     private Dieta dieta;
     private Usuario paciente;
     private String pacienteStr;
+    private List<Dieta> listaDietas;
 
     public DietaBean() {
         this.dieta = new Dieta();
         this.paciente = new Usuario();
         this.pacienteStr = "";
+        this.listaDietas = new ArrayList<>();
+    }
+
+    public void adicionarDietaLista() {
+        Usuario u = this.usuarioBean.retornaUsuarioById(this.pacienteStr);
+        this.calendarioAlimentacaoBean.cadastrarCalendarioAlimentacao(u);
+        this.dieta.setCalendariAlimentacao(this.calendarioAlimentacaoBean.retornaCalendarios(u));
+        this.dieta.setNutricionistaResponsavel(this.usuarioBean.getUsuario());
+        this.dieta.setDataDieta(new Date());
+        this.listaDietas.add(this.dieta);
+        this.dieta = new Dieta();
     }
 
     public void atribuirPaciente() {
@@ -41,20 +56,44 @@ public class DietaBean implements Serializable {
         em.close();
     }
 
-    public void cadastrarDieta() {
-        Usuario u = this.usuarioBean.retornaUsuarioById(this.pacienteStr);
-        this.calendarioAlimentacaoBean.cadastrarCalendarioAlimentacao(u);
-        this.dieta.setCalendariAlimentacao(this.calendarioAlimentacaoBean.retornaCalendarios(u));
-        this.dieta.setNutricionistaResponsavel(this.usuarioBean.getUsuario());
-        this.dieta.setDataDieta(new Date());
+    public void cadastrarDietas() {
+        if (!isAllDiasCadastrados()) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage("Oooops!", "Você não cadastrou dieta para todos os dias!"));
+            return;
+        }
+
         EntityManager em = JPAUtil.getEntityManager();
         em.getTransaction().begin();
-        em.merge(this.dieta);
+        for (Dieta diet : this.listaDietas) {
+            em.merge(diet);
+        }
         em.getTransaction().commit();
         em.close();
-        this.dieta = new Dieta();
+
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage("Sucesso!", "Dieta cadastrada!"));
+    }
+
+    public Boolean isDiaCadastrado(String dia) {
+        for (Dieta listaDieta : this.listaDietas) {
+            for (CalendarioAlimentacao calendarioAlimentacao : listaDieta.getCalendariAlimentacao()) {
+                if (calendarioAlimentacao.getDiaSemana().equalsIgnoreCase(dia)) {
+                    return Boolean.TRUE;
+                }
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    public Boolean isAllDiasCadastrados() {
+        Integer cont = 0;
+        for (String dia : this.calendarioAlimentacaoBean.retornaDiasSemana()) {
+            if (isDiaCadastrado(dia)) {
+                cont++;
+            }
+        }
+        return cont == 7;
     }
 
     public UsuarioBean getUsuarioBean() {
@@ -95,6 +134,14 @@ public class DietaBean implements Serializable {
 
     public void setPacienteStr(String pacienteStr) {
         this.pacienteStr = pacienteStr;
+    }
+
+    public List<Dieta> getListaDietas() {
+        return listaDietas;
+    }
+
+    public void setListaDietas(List<Dieta> listaDietas) {
+        this.listaDietas = listaDietas;
     }
 
 }
